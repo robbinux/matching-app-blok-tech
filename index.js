ac1ea2b35b20f0480b88c75f8d7d76ec46c1baf7
robbinux <robbin-jansen@hotmail.com>
2021-02-16 15:35:47 +0100

    onnodige code verwijderd

--- index.js
+++ index.js
@@ -6,34 +6,6 @@ const port = 3000
 
 
 
-app.use(express.static('static'));
-
-
-
-app.get('/', (req, res) => {​​​​
-
-  res.send('Hello World!');
-
-}​​​​);
-
-app.get('/movies', (req,res) => {​​​​
-
-    res.send('A list of movies');
-
-}​​​​);
-
-app.get('/movies/:movieId', (req,res) => {​​​​
-
-  res.send(`<h1>Detailpage of movie ${​​​​req.params.movieId}​​​​ </h1>`);
-
-}​​​​);
-
-app.use(function (req, res, next) {​​​​
-
-  res.status(404).send("Sorry can't find that!");
-
-}​​​​);
-
 
 
 app.listen(3000, () => {​​​​
