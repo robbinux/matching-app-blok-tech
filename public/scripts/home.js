0dcd64e529a341a960d0ffc50515d4f75198c7e3
robbinux <robbin-jansen@hotmail.com>
2021-04-03 04:06:36 +0200

    reset to base

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,12 +1,12 @@
-// Swipe Left;
-const buttonX = document.querySelector(".x");
+// // Swipe Left;
+// const buttonX = document.querySelector(".x");
 
 
-buttonX.addEventListener("click", function() {
-    console.log("swipe left");
-});
+// buttonX.addEventListener("click", function() {
+//     console.log("swipe left");
+// });
 
-//Swipe Right
+// //Swipe Right
 
 
 // drag function for slider
