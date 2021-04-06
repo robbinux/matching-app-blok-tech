f083d18b19fcff79762a9f04a9029bfe61b4c811
robbinux <robbin-jansen@hotmail.com>
2021-04-06 03:15:11 +0200

    adding env sample and code cleanup

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,8 +1,3 @@
 
 const button = document.querySelector(".submit");
 
-// button.addEventListener("click", function() {
-//   document.querySelector("form").classList.add("formhidden");
-//   document.querySelector(".card").classList.remove("cardhidden");
-//   window.location.reload(true);
-// });
